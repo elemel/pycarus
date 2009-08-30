1d7ae09080c4b4f236aa72069fbf151d101b396a
Mikael Lind <elemel@elemel.se>
2009-08-30 21:19:10 +0200

    - Added placeholder graphics from "Landscape with the Fall of Icarus" by   Hans Bol. - Added Icarus actor. Fly on your wings like an eagle using the arrow keys. - Started using Rabbyt sprites.

--- lib/feather/config.py
+++ lib/feather/config.py
@@ -1,3 +1,8 @@
 fps = True
 fullscreen = True
 gravity = 0, -10
+position_iterations = 10
+velocity_iterations = 10
+icarus_lift_force = 15
+icarus_side_force = 5
+icarus_air_resistance = 1
--- lib/feather/main.py
+++ lib/feather/main.py
@@ -4,6 +4,7 @@ from Box2D import *
 from feather import config
 import pyglet
 from pyglet.gl import *
+import rabbyt
 import sys
 
 class Screen(object):
@@ -17,22 +18,17 @@ class Screen(object):
 class TitleScreen(Screen):
     def __init__(self, window):
         super(TitleScreen, self).__init__(window)
-        self.image = pyglet.resource.image('images/title.jpg')
-        self.image.anchor_x = self.image.width // 2
-        self.image.anchor_y = self.image.height // 2
-        self.sprite = pyglet.sprite.Sprite(self.image)
-
-    def delete(self):
-        self.sprite.delete()
-        super(TitleScreen, self).delete()
+        texture = pyglet.resource.texture('images/title.jpg')
+        self.sprite = rabbyt.Sprite(texture)
 
     def on_draw(self):
         self.window.clear()
-        self.sprite.scale = max(self.window.width / self.image.width,
-                                self.window.height / self.image.height)
+        scale_x = self.window.width / self.sprite.texture.width
+        scale_y = self.window.height / self.sprite.texture.height
+        self.sprite.scale = max(scale_x, scale_y)
         self.sprite.x = self.window.width // 2
         self.sprite.y = self.window.height // 2
-        self.sprite.draw()
+        self.sprite.render()
         return pyglet.event.EVENT_HANDLED
 
     def on_key_press(self, symbol, modifiers):
@@ -43,18 +39,104 @@ class TitleScreen(Screen):
             GameScreen(self.window)
         return pyglet.event.EVENT_HANDLED
 
+class Actor(object):
+    def step(self, dt):
+        pass
+
+    def draw(self):
+        pass
+
+class Icarus(Actor):
+    def __init__(self, game_screen, position=(0, 0)):
+        self.game_screen = game_screen
+        self._init_body(position)
+        self._init_sprite()
+        self._keys = set()
+        self._power = 1
+        self._facing = 1
+
+    def _init_body(self, position):
+        body_def = b2BodyDef()
+        body_def.position = position
+        self.body = self.game_screen.world.CreateBody(body_def)
+        shape_def = b2CircleDef()
+        shape_def.radius = 0.5
+        shape_def.density = 1
+        self.body.CreateShape(shape_def)
+        self.body.SetMassFromShapes()
+
+    def _init_sprite(self):
+        texture = pyglet.resource.texture('images/icarus.png')
+        self.sprite = rabbyt.Sprite(texture, scale=0.02)
+
+    def delete(self):
+        self.game_screen.world.DestroyBody(self.body)
+
+    def draw(self):
+        self.sprite.xy = self.body.position.tuple()
+        self.sprite.scale_x = self._facing * abs(self.sprite.scale_x)
+        self.sprite.render()
+
+    def step(self, dt):
+        up = pyglet.window.key.UP in self._keys
+        left = pyglet.window.key.LEFT in self._keys
+        right = pyglet.window.key.RIGHT in self._keys
+        if left ^ right:
+            self._facing = right - left
+        lift_force = up * self._power * config.icarus_lift_force
+        side_force = (right - left) * config.icarus_side_force
+        left = pyglet.window.key.LEFT in self._keys
+        right = pyglet.window.key.LEFT in self._keys
+        air_force = -(self.body.linearVelocity * config.icarus_air_resistance)
+        self.body.ApplyForce(b2Vec2(side_force, lift_force) + air_force,
+                             self.body.position)
+
+    def on_key_press(self, symbol, modifiers):
+        self._keys.add(symbol)
+    
+    def on_key_release(self, symbol, modifiers):
+        self._keys.discard(symbol)
+
+class Cloud(Actor):
+    pass
+
 class GameScreen(Screen):
     def __init__(self, window):
         super(GameScreen, self).__init__(window)
         self._init_world()
         self._clock_display = pyglet.clock.ClockDisplay()
+        self._icarus = Icarus(self)
+        self.time = 0
+        self.dt = 1 / 60
+        self.world_time = 0
+        pyglet.clock.schedule_interval(self.step, self.dt)
+
+    def delete(self):
+        pyglet.clock.unschedule(self.step)
+        super(GameScreen, self).delete()
+
+    def step(self, dt):
+        self.time += dt
+        while self.world_time + self.dt <= self.time:
+            self.world_time += self.dt
+            self._icarus.step(self.dt)
+            self.world.Step(self.dt, config.position_iterations,
+                            config.velocity_iterations)
 
     def _init_world(self):
         aabb = b2AABB()
+        aabb.lowerBound = -100, -100
+        aabb.upperBound = 100, 100
         self.world = b2World(aabb, config.gravity, True)
 
     def on_draw(self):
         self.window.clear()
+        glPushMatrix()
+        glTranslatef(self.window.width // 2, self.window.height // 2, 0)
+        scale = self.window.height / 10
+        glScalef(scale, scale, scale)
+        self._icarus.draw()
+        glPopMatrix()
         if config.fps:
             self._clock_display.draw()
         return pyglet.event.EVENT_HANDLED
@@ -62,10 +144,17 @@ class GameScreen(Screen):
     def on_key_press(self, symbol, modifiers):
         if symbol == pyglet.window.key.ESCAPE:
             self.delete()
+        else:
+            self._icarus.on_key_press(symbol, modifiers)
+        return pyglet.event.EVENT_HANDLED
+
+    def on_key_release(self, symbol, modifiers):
+        self._icarus.on_key_release(symbol, modifiers)
         return pyglet.event.EVENT_HANDLED
 
 def main():
     window = pyglet.window.Window(fullscreen=config.fullscreen)
+    rabbyt.set_default_attribs()
     TitleScreen(window)
     pyglet.app.run()
 
